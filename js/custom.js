c487492ed3acea864d7cb037306af0f0df7545ee
veratechnz <jared@veratech.co.nz>
2014-07-04 17:52:07 +1200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+// //Custom JS For Bootstrap:
+
+// $(document).ready(function(){
+
+
+// 	//Declare a variable that relates to the grey ID on the document object model. 
+// 	var timetext = $('#grey');
+
+// //Hide timetext variable for the up coming fadein
+// 	$(timetext).hide();	
+
+// 	alert('We are alive!');
+
+// 	$('#red').click(function(){
+
+// 		$(this).hide(5000);
+
+// //This shows us that Jquery can traverse the DOM. ".next" gives us access to an elements next sibling
+
+// 		$(this).next().fadeOut(20000);
+
+// 		$(timetext).fadeIn(20000);
+
+
+// 	});
+
+
+
+// });
