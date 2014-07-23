329f5128b59eaca0048d6c224e9ab74329c388bf
veratech <jared@veratech.co.nz>
2014-07-23 17:16:29 +1200

    230714

---
+++ inc/logo.nav.php
@@ -0,0 +1,26 @@
+ <!-- Top Logo Section Start -->
+    <section id="logo">
+        <div class="row" id="logo">
+            <div class="col-md-6 col-md-offset-3">
+                <img src="images/logo.jpg" id="logo" class="img-responsive center-block" alt="Welcome To Urban Calm Logo">
+            </div>
+        </div>
+    </section>
+    <!-- Top Logo Section End -->
+    <!-- Top Menu Section Start -->
+    <section id="navigation" class="voffset">
+        <div class="row nav-height">
+            <div class="col-md-10 col-md-offset-1 nav-height">
+                <nav id="navee" class="nav-height">
+                  <ul id="nav-list" class="nav-height">
+                    <li class="cent-fix nav-height "><a href="#">ABOUT</a></li>
+                    <li class="cent-fix nav-height "><a href="#">BOOKINGS</a></li>
+                    <li class="cent-fix nav-height "><a href="#">PHILOSOPHY</a></li>
+                    <li class="cent-fix nav-height "><a href="#">EVENTS</a></li>
+                    <li class="cent-fix nav-height "><a href="#">RESOURCES</a></li>
+                    <li class="cent-fix nav-height "><a href="#">CONTACT</a></li>
+                  </ul>
+                </nav>​
+            </div>
+        </div>
+    </section>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Php Head Begins -->
+    <?php include 'inc/head.inc.php';?>
+    <!-- Php Head Ends -->
+  <body>
+    <!-- container begins -->  
+    <div class="container">  
+    <!-- Logo Nav Begins -->
+    <?php include 'inc/logonav.inc.php';?>
+    <!-- Logo Nav Ends -->
+    <!-- Home Page Slider Section -->
+        <div class="row voffset3">
+          <div class="col-md-4 col-md-offset-4" id="main-slider">
+              <img src="images/slider-1.jpg" class="center-block text-center" alt="Welcome To Urban Calm">
+          </div>
+        </div>
+    <!-- Home Page Slider Section Ends -->
+    </div> 
+    <!-- /container ends -->
+    <!-- Js Scripts Begin -->
+    <?php include 'inc/js.inc.php';?>
+    <!-- Js Scripts End -->
+  </body>
+</html>
