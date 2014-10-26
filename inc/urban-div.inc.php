5957ea7d22459725ea4b015d4fe75a0911cb1a70
veratech <jared@veratech.co.nz>
2014-10-27 12:09:52 +1300

    271014

--- inc/urban-div.inc.php
+++ inc/urban-div.inc.php
@@ -1,9 +1,10 @@
 <div class="row vboffset1 hidden-xs">
 	<div class="col-md-12">
           <!-- Urban Image Div -->
-          <div class="urban-div vboffset1">
-            <img src="images/urban-div.png" alt="" class="center-block img-responsive vboffset1">
+          <div class="urban-div">
+            <img src="images/urban-div.png" alt="" class="center-block img-responsive">
           </div>
+          <p class="text-center voffset1 nusize">PH: 03 477 2277 | Level 1, Clarion Building, 286 Princess Street Dunedin</p>
           <!-- Urban Image Div -->
 	</div>
 </div>
